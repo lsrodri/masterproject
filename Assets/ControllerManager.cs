bc8ea7132b9a2ce0472a54bba9766b2cf3ce06be
lucasiq <lrodri@terpmail.umd.edu>
2018-07-10 17:09:11 +0200

    Created first stable interaction with controllers and Touchstone data

--- Assets/ControllerManager.cs
+++ Assets/ControllerManager.cs
@@ -17,6 +17,8 @@ public class ControllerManager : MonoBehaviour {
     public string outputDataPath;
     public Text outputUserResponse;
     public GameObject preExperimentCanvas;
+    public GameObject confirmationCanvas;
+    public GameObject invalidInputCanvas;
     public GameObject leftStimulus;
     public GameObject rightStimulus;
     public Sprite lenghtSprite;
@@ -24,9 +26,11 @@ public class ControllerManager : MonoBehaviour {
 
     //Initializing as 0 for direct scene testing as this variable is set at the index scene
     private string currentUserId = "1";
-    private SpriteRenderer spriteRenderer;
+    private SpriteRenderer leftSpriteRenderer;
+    private SpriteRenderer rightSpriteRenderer;
     private SteamVR_TrackedObject trackedObject;
     private SteamVR_Controller.Device device;
+    private bool userHasEstimated = false;
 
     RunExperiment _runExperiment;
 
@@ -43,22 +47,64 @@ public class ControllerManager : MonoBehaviour {
         float controllerValue = device.GetAxis().y;
         if (device.GetPress(SteamVR_Controller.ButtonMask.Touchpad))
         {
-            if (controllerValue > 0 && userResponse < 100)
+            // Flag that user has entered a value for this trial
+            userHasEstimated = true;
+
+            /* If user presses the wheel while confirmation screen is open 
+             * the application hides the confirmation screen
+             * to allow changing the answer
+            */
+            if (confirmationCanvas.activeSelf)
             {
-                tempUserResponse+= 0.1;
-            } else if (controllerValue < 0 && userResponse > 0)
-            {
-                tempUserResponse -= 0.1;
+                confirmationCanvas.SetActive(false);
             }
+
+            // Closing the invalid input warning when the user clicks the wheel as it defines a value
+            if (invalidInputCanvas.activeSelf)
+            {
+                invalidInputCanvas.SetActive(false);
+            }
+
+            // Only allowing value to change if the experiment has already started
+            if (!preExperimentCanvas.activeSelf)
+            {
+                // Allowing the value to be incremented when controller input is positive and calculated value < 100
+                if (controllerValue > 0 && userResponse < 100)
+                {
+                    tempUserResponse += 0.1;
+                }
+                // Allowing the value to be decreased when controller input is positive and calculated value > 0
+                else if (controllerValue < 0 && userResponse > 0)
+                {
+                    tempUserResponse -= 0.1;
+                }
+            }
+            
             userResponse = (int)System.Math.Round(tempUserResponse, System.MidpointRounding.AwayFromZero);
             outputUserResponse.text = userResponse.ToString() + "%";
             PlayerPrefs.SetInt("userResponse", userResponse);
-        } else if (device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger))
+        }
+        else if (device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger))
         {
+            // The Pre-experiment canvas is active before the experiment starts
             if (preExperimentCanvas.activeSelf)
             {
                 StartExperiment(currentUserId, 0, 0, false);
                 preExperimentCanvas.SetActive(false);
+            }
+            else if (userHasEstimated == true)
+            {
+                if (currentTrialIndex != -1 && !confirmationCanvas.activeSelf)
+                {
+                    confirmationCanvas.SetActive(true);
+                }
+                else if (currentTrialIndex != -1 && confirmationCanvas.activeSelf)
+                {
+                    NextTrial();
+                }
+            } else if (userHasEstimated == false)
+            {
+                invalidInputCanvas.SetActive(true);
             }
         }
     }
@@ -103,25 +149,42 @@ public class ControllerManager : MonoBehaviour {
 
         Debug.Log("<color=#E91E63>Current trial : " + currentTrialIndex + "</color>");
 
-        
+        leftSpriteRenderer = leftStimulus.GetComponent<SpriteRenderer>();
+        rightSpriteRenderer = rightStimulus.GetComponent<SpriteRenderer>();
+
+        // Resetting values and outputs for the next trial
+        confirmationCanvas.SetActive(false);
+        userHasEstimated = false;
+        outputUserResponse.text = "";
+        userResponse = 50;
+        PlayerPrefs.SetInt("userResponse", userResponse);
+        tempUserResponse = userResponse;
 
         if (property == "Length")
         {
             // Scale used by this property to fit the paper and make the right positioning possible
             propertySize = 0.5f;
 
-            spriteRenderer = GetComponent<SpriteRenderer>();
-            spriteRenderer.sprite = lenghtSprite;
-
+            
+            leftSpriteRenderer.sprite = lenghtSprite;
             leftStimulus.transform.localScale = new Vector3(propertySize, propertySize, propertySize);
-            leftStimulus.transform.position = new Vector3(0.048f, 0.012f, 0.063f);
-
+            leftStimulus.transform.localPosition = new Vector3(0.048f, 0.012f, 0.063f);
+                        
+            rightSpriteRenderer.sprite = lenghtSprite;
             rightStimulus.transform.localScale = new Vector3(propertySize, propertySize * ratio, propertySize);
-            rightStimulus.transform.position = new Vector3(-0.164f, 0.012f, 0.043f);
+            rightStimulus.transform.localPosition = new Vector3(-0.164f, 0.012f, 0.043f);
 
         } else if (property == "Area")
         {
-            rightStimulus.transform.localScale = new Vector3(ratio, ratio, ratio);
+            propertySize = 0.2f;
+
+            leftSpriteRenderer.sprite = areaSprite;
+            leftStimulus.transform.localScale = new Vector3(propertySize, propertySize, propertySize);
+            leftStimulus.transform.localPosition = new Vector3(0.061f, 0.012f, 0.063f);
+        
+            rightSpriteRenderer.sprite = areaSprite;
+            rightStimulus.transform.localScale = new Vector3(propertySize * ratio, propertySize * ratio, propertySize * ratio);
+            rightStimulus.transform.localPosition = new Vector3(-0.164f, 0.012f, 0.033f);
         }
         
 
@@ -145,6 +208,8 @@ public class ControllerManager : MonoBehaviour {
     void ExperienceFinished()
     {
         Debug.Log("The experience is finished");
+        // Calling another scene to avoid further interaction and give user a message
+        UnityEngine.SceneManagement.SceneManager.LoadScene(2);
     }
 
 
